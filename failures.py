b9d9f1ee17875780b29c7ef820f747944a0e6464
Bonan Shen <shenbonan2@gmail.com>
2022-11-14 21:22:37 -0800

    add functions to simulate drone failures

--- failures.py
+++ failures.py
@@ -1,9 +1,11 @@
 import math
 import numpy as np
+from droneconfig import Drone
 
 class FailureHandling:
     Y_OFFSET = 10
     Z_OFFSET = 2
+    SIM_LIGHT_FAILURES = 5
 
     @classmethod
     def handle_deployment(cls, deployment:  dict[int, list[int]], overhead: float) -> dict[int, list[int]]:
@@ -28,7 +30,7 @@ class FailureHandling:
         return deployment
 
     @classmethod
-    def handle_poses(cls, poses: list[list[int]], overhead: float) -> list[list[int]]:
+    def handle_poses(cls, poses: list[list[int]], overhead: float):
         """_summary_
 
         Args:
@@ -43,6 +45,16 @@ class FailureHandling:
         maxy = np.max(np.array(poses)[:,1]) + cls.Y_OFFSET
         maxz = np.max(np.array(poses)[:,2])
         maxx = np.max(np.array(poses)[:,0])
+        pose_ids = []
         for i in range(n_extra):
             poses.append([maxx, maxy, maxz - i * cls.Z_OFFSET])
-        return poses
+            pose_ids.append(len(poses)-1)
+        return poses, pose_ids
+
+    @classmethod
+    def sim_light_failures(cls, drone: Drone):
+        drone.light = False
+    
+    @classmethod
+    def sim_rotor_failures(cls, drone: Drone):
+        drone.rotor = False
