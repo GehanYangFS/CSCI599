f39dbc8890be3933c2f54ca3c281a90b1cfd4f1c
Bonan Shen <shenbonan2@gmail.com>
2022-11-19 22:21:24 -0800

    add failure and multiframe control flags

--- flags.py
+++ flags.py
@@ -1,2 +1,4 @@
 Flag_ue_executable_file = r'C:\Users\shenb\Downloads\CSCI-599\Blocks\WindowsNoEditor\Blocks.exe'
 Flag_ue_executable_settings_path = 'C:/Users/shenb/Documents/AirSim/settings.json'
+Flag_failures = False
+Flag_multiframe = True
